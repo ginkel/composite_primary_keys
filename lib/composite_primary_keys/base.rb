6978b34fa1a44087ebbe8ae0dbc42bf99587ea77
Tim Morgan <git@timothymorgan.info>
2010-09-18 12:12:45 -0700

    Rails 3 fix

--- lib/composite_primary_keys/base.rb
+++ lib/composite_primary_keys/base.rb
@@ -135,7 +135,7 @@ module ActiveRecord
       def update_attribute(name, value)
         raise ActiveRecordError, "#{name.to_s} is marked as readonly" if self.class.readonly_attributes.include? name.to_s
 
-        changes = record_update_timestamps || {}
+        changes = {}
 
         if name
           name = name.to_s
